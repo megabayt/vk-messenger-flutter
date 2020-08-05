1d1362b93afd05d54553420c322a6651cd7a1657
Артемий Мартьянов <amartyanov@dclab.local>
2020-08-05 16:44:59 +0300

    add avatar and surname

--- lib/widgets/conversation_tile.dart
+++ lib/widgets/conversation_tile.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:vk_messenger_flutter/models/vk_conversations.dart' show Item;
+import 'package:vk_messenger_flutter/store/chats_store.dart';
 import 'package:vk_messenger_flutter/widgets/skeleton.dart';
 
 class ConversationTile extends StatelessWidget {
   final Item _item;
 
+  get _peerId {
+    return _item?.conversation?.peer?.id;
+  }
+
   ConversationTile(this._item);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ChatsStore>(context);
+
+    final profiles = provider.data?.response?.profiles ?? [];
+
+    final profile = profiles.firstWhere(
+      (profile) => profile.id == _peerId,
+      orElse: () => null
+    );
+
     if (_item == null) {
       return Padding(
         padding: const EdgeInsets.symmetric(vertical: 15.0),
@@ -17,8 +32,9 @@ class ConversationTile extends StatelessWidget {
     }
     return ListTile(
       leading: CircleAvatar(
-          backgroundImage: NetworkImage('https://unsplash.it/150/200?random')),
-      title: Text(_item.lastMessage.text),
+          backgroundImage: NetworkImage(profile != null ? profile.photo50 : 'https://unsplash.it/150/200?random')),
+      title: Text(profile != null ? profile.lastName : _peerId.toString()),
+      subtitle: Text(_item?.lastMessage?.text),
     );
   }
 }
